67e077406888d012e802d50326b17b337f69f9b2
yuler <is.yuler@gmail.com>
2022-04-10 17:38:49 +0800

    Add prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import {Controller, Get} from '@nestjs/common'
 import {AppService} from './app.service'
+import {PrismaService} from './prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get('/test-prisma-service')
+  Test() {
+    return this.prismaService.user.findMany()
   }
 }
